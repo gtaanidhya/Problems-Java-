3f102e95cb164297f94ef4a504ffbbb710302c81
Anidhya Gupta <guptaanidhya2000.fb@gmail.com>
2025-03-05 08:10:31 +0530

    Tower of Hanoi

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,15 @@
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        towerOfHanoi(3, 'A', 'C', 'B');
+    }
+
+    private static void towerOfHanoi(int n, char source, char dest, char aux) {
+        if (n == 0) {
+            return;
+        }
+        towerOfHanoi(n - 1, source, aux, dest);
+        System.out.println("Move disk from " + source + " to " + dest);
+        towerOfHanoi(n - 1, aux, dest, source);
+
+    }
+}
