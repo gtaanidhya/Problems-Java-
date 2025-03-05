03560186ffb613eae7f524a6c779a0a786c45c13
guptaanidhya2000.fb@gmail.com <guptaanidhya2000.fb@gmail.com>
2025-03-05 10:34:09 +0530

    new backtracking problem

---
+++ Recursion/allStrings0toK.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class allStrings0toK {
+    int[] A;
+
+    public allStrings0toK(int n) {
+        A = new int[n];
+    }
+
+    public void K_strings(int n, int k) {
+        if (n <= 0) {
+            System.out.println(Arrays.toString(A));
+        } else {
+            for (int j = 0; j <= k; j++) {
+                A[n - 1] = j;
+                K_strings(n - 1, k);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int k = s.nextInt();
+        allStrings0toK i = new allStrings0toK(n);
+        i.K_strings(n, k);
+        s.close();
+    }
+}
+// O(k^n) time complexity
--- Recursion/isArraySorted.java
+++ Recursion/isArraySorted.java
@@ -1,3 +1,5 @@
+//Backtracking problem
+
 public class isArraySorted {
     public static void main(String[] args) {
         int[] arr = { 1, 3, 4, 7, 6 };
